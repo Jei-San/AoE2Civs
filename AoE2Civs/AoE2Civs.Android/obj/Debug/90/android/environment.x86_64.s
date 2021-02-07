9e3ff2d6431fdbf3786ae0dc13fa2be756c94fbc
Jei-San <67445580+Jei-San@users.noreply.github.com>
2021-02-07 17:25:25 -0600

    hardcoded images into the android project

--- AoE2Civs/AoE2Civs.Android/obj/Debug/90/android/environment.x86_64.s
+++ AoE2Civs/AoE2Civs.Android/obj/Debug/90/android/environment.x86_64.s
@@ -72,7 +72,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",@progbits,1
 	.type	.L.env.str.8, @object
 .L.env.str.8:
-	.asciz	"5522ef1c-b75a-4f0d-a324-db5247f9f458"
+	.asciz	"775e897c-f74e-4550-ad11-4b93714e43a6"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",@progbits,1
 	.type	.L.env.str.9, @object
--- AoE2Civs/AoE2Civs/ViewModel/MainPageModel.cs
+++ AoE2Civs/AoE2Civs/ViewModel/MainPageModel.cs
@@ -47,13 +47,18 @@ namespace AoE2Civs.ViewModel
             if (response.StatusCode == System.Net.HttpStatusCode.OK)
             {
                 Root result = JsonConvert.DeserializeObject<Root>(response.Content);
+                //Created a new list of collection from Civilization
                 var NewCivList = new ObservableCollection<Civilization>();
                 Civilizations = new ObservableCollection<Civilization>(result.civilizations);
+                //Created a foreach to check every var in Civilizations object that calls the imagePath inside
+                //Civilization class and assigned the value to AssignedPicture with properties of type string
+                //that came in the list of name then called the new collection and added the civ parameter to it
                 foreach(var civ in Civilizations)
                 {
                     civ.imagePath = AssignedPicture(civ.name);
                     NewCivList.Add(civ);
                 }
+                //Assigned NewCivList value to Civilizations
                 Civilizations = NewCivList;
                 civList = new ObservableCollection<Civilization>(result.civilizations);
                 IsLoaded = true;
@@ -87,17 +92,103 @@ namespace AoE2Civs.ViewModel
             if (civList != null)
                 civList.Clear();
         }
+        //Made a function with the AssignedPicture and added a parameter called civName of type string
+        //created a variable called CivImage which will have the .png value to pass it on to the xaml with a switch statement
         public string AssignedPicture(string civName)
         {
             var CivImage = "";
             switch(civName)
-            {
-               case "Aztecs":
+            {//generizicing with sqlite
+                case "Aztecs":
                     CivImage = "Aztecs.png";
                     break;
-               case "Mayans":
-                    CivImage = "Mayans.png";
+                case "Britons":
+                    CivImage = "Britons.png";
                    break;
+                case "Byzantines":
+                    CivImage = "Byzantines.png";
+                    break;
+                case "Celts":
+                    CivImage = "Celts.png";
+                    break;
+                case "Chinese":
+                    CivImage = "Chinese.png";
+                    break;
+                case "Franks":
+                    CivImage = "Franks.png";
+                    break;
+                case "Goths":
+                    CivImage = "Goths.png";
+                    break;
+                case "Huns":
+                    CivImage = "Huns.png";
+                    break;
+                case "Japanese":
+                    CivImage = "Japanese.png";
+                    break;
+                case "Koreans":
+                    CivImage = "Koreans.png";
+                    break;
+                case "Mayans":
+                    CivImage = "Mayans.png";
+                    break;
+                case "Mongols":
+                    CivImage = "Mongols.png";
+                    break;
+                case "Persians":
+                    CivImage = "Persians.png";
+                    break;
+                case "Saracens":
+                    CivImage = "Saracens.png";
+                    break;
+                case "Spanish":
+                    CivImage = "Spanish.png";
+                    break;
+                case "Teutons":
+                    CivImage = "Teutons.png";
+                    break;
+                case "Turks":
+                    CivImage = "Turks.png";
+                    break;
+                case "Vikings":
+                    CivImage = "Vikings.png";
+                    break;
+                case "Berbers":
+                    CivImage = "Berbers.png";
+                    break;
+                case "Burmese":
+                    CivImage = "Burmese.png";
+                    break;
+                case "Ethiopians":
+                    CivImage = "Ethiopians.png";
+                    break;
+                case "Incas":
+                    CivImage = "Incas.png";
+                    break;
+                case "Indians":
+                    CivImage = "Indians.png";
+                    break;
+                case "Italians":
+                    CivImage = "Italians.png";
+                    break;
+                case "Khmer":
+                    CivImage = "Khmer.png";
+                    break;
+                case "Magyars":
+                    CivImage = "Magyars.png";
+                    break;
+                case "Malians":
+                    CivImage = "Malians.png";
+                    break;
+                case "Portuguese":
+                    CivImage = "Portuguese.png";
+                    break;
+                case "Slavs":
+                    CivImage = "Slavs.png";
+                    break;
+                case "Vietnamese":
+                    CivImage = "Vietnamese.png";
+                    break;
             }
             return CivImage;
         } 
@@ -108,10 +199,7 @@ namespace AoE2Civs.ViewModel
         }
         private void OnPropertyChanged(string propertyName)
         {
-            if (PropertyChanged != null)
-            {
-                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
-            }
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
     }
 }
